b92400f78a9ee6cd841081a23d85b0dbb55f441e
Michel Klein <michel.silva@eduzz.com>
2022-12-13 13:45:48 -0300

    feat: add products mutations

--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -7,6 +7,16 @@ module.exports = {
     },
     productsByPrice: (_, args) => {
         return productsModel.getProductsByPrice(args.min, args.max);
+    },
+    product: (_, args) => {
+        return productsModel.getProductById(args.id);
+    },
+Mutation: {
+    addNewProduct: (_, args) => {
+        return productsModel.addNewProduct(args.id, args.description, args.price);
+    },
+    addNewProductReview: (_, args) => {
+        return productsModel.addNewProductReview(args.id, args.rating, args.comment);
     }
   }
 };
